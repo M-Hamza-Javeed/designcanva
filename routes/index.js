0dc7deda2c50e41f0ea4949c9a6dcf1306866ee6
Hamza <ha42226@gmail.com>
2021-08-18 10:46:52 -0700

    improvements

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 const gradstop = require('gradstop');
-var colors = require('nice-color-palettes/1000');
+var colors = require('nice-color-palettes/100');
 var router = express.Router();
 var size=5;var counter=0;var color="";var page="";
 
@@ -8,15 +8,17 @@ var size=5;var counter=0;var color="";var page="";
 async function ColorObj(gradient,node){
   counter++;
   if(counter==1){
-  color+="<p style='color:#fff;padding:10px 20px;background: #660c41;'>"+node+"</p>";
-  color+="<div style='padding:20px;margin:20px;background:#f7f9f8;box-shadow: 1px 1px 5px #14383eb8;display:flex;flex-wrap: wrap;'>";}
-  color+="<div style='display: flex;margin:10px 25px;padding:5px;box-shadow: 1px 1px 5px;flex-direction:column;justify-content: center;align-items: center;'>";
+  color+="<p class='index' >"+node+"</p>";
+  color+="<div class='palette-container' >";}
+  color+="<div class='palette-container-inner' >";
   color+="<div style='display: flex;'>";
   for(var i=0;i<gradient.length;i++){
-  if(i==size-1){color+="<div style='background:"+gradient[i].toString()+";width: 50px;height: 50px;margin-bottom:5px;'></div>";
-  }else{color+="<div style='background:"+gradient[i].toString()+";width: 50px;height: 50px;margin-bottom:5px;'></div>";}}
-  color+="</div>";color+="<button onclick='upload(event)' style='margin-top:5px;width:70px;height:30px;'>Upload</button></div>";
-  if(counter>10){color+="</div>";}}
+  if(i==size-1){color+="<div style='background:"+gradient[i].toString()+";width: 50px;height: 50px;'></div>";
+  }else{color+="<div style='background:"+gradient[i].toString()+";width: 50px;height: 50px;'></div>";}}
+  color+="</div>";
+  color+="</div>";
+  if(counter>10){color+="</div>";}
+}
   
 
   
@@ -35,7 +37,8 @@ async function ColorObj(gradient,node){
       ColorObj(gradstop({stops:size,inputFormat: 'hex',colorArray:[colors[i][2],colors[i][3]]}),i);
       ColorObj(gradstop({stops:size,inputFormat: 'hex',colorArray:[colors[i][2],colors[i][4]]}),i);
       ColorObj(gradstop({stops:size,inputFormat: 'hex',colorArray:[colors[i][3],colors[i][4]]}),i);
-      counter=0;}page="<div style='display:flex;flex-wrap:wrap-reverse;flex-flow: wrap;'>"+color+"</div>";}
+      counter=0;}page="<div style='display:flex;background:#733f1d;\
+      margin:80px;flex-wrap:wrap-reverse;flex-flow: wrap;'>"+color+"</div>";}
   
   function render(req,res,data) { 
     res.render('index', {'page':data});}
